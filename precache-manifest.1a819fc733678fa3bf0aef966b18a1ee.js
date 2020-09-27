98691ea4fc7ac97dcbed7a8284703381e096e0e5
Hasnain <our.hasnain22@gmail.com>
2020-09-27 14:46:28 +0500

    Updates

---
+++ precache-manifest.1a819fc733678fa3bf0aef966b18a1ee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05627d64867f0541162786039cf100e0",
+    "url": "/Infinity/index.html"
+  },
+  {
+    "revision": "4234d07f834f311e23d7",
+    "url": "/Infinity/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "e6131e7e44ef5086f900",
+    "url": "/Infinity/static/css/main.8961bf5c.chunk.css"
+  },
+  {
+    "revision": "4234d07f834f311e23d7",
+    "url": "/Infinity/static/js/2.252dd7dd.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Infinity/static/js/2.252dd7dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6131e7e44ef5086f900",
+    "url": "/Infinity/static/js/main.7dc3b831.chunk.js"
+  },
+  {
+    "revision": "18888c0856ad03ac3c76",
+    "url": "/Infinity/static/js/runtime-main.dcd1ca7b.js"
+  },
+  {
+    "revision": "e969ece6b11cd5782fd1e309d3c43252",
+    "url": "/Infinity/static/media/android.e969ece6.jpg"
+  },
+  {
+    "revision": "646701fc99bf422100961807c98001cd",
+    "url": "/Infinity/static/media/app.646701fc.jpg"
+  },
+  {
+    "revision": "3dfa54e1417cb377e6d58b3880927cd6",
+    "url": "/Infinity/static/media/digital.3dfa54e1.jpg"
+  },
+  {
+    "revision": "724b3434ae75d1013d57ad7c9cc6f878",
+    "url": "/Infinity/static/media/infinity.724b3434.svg"
+  },
+  {
+    "revision": "04abe052bb8cbf54cb6f04951750aa74",
+    "url": "/Infinity/static/media/ios.04abe052.jpg"
+  },
+  {
+    "revision": "d043e42ecbf27a0737a6e341d33f2316",
+    "url": "/Infinity/static/media/landing.d043e42e.svg"
+  },
+  {
+    "revision": "cc745dfff98669f28737556583f2b909",
+    "url": "/Infinity/static/media/marketing.cc745dff.jpg"
+  },
+  {
+    "revision": "65855ac7fce3ea6f254e17e6a413305d",
+    "url": "/Infinity/static/media/port.65855ac7.svg"
+  },
+  {
+    "revision": "5b4b506578c653c5c054b42be6c8ac2d",
+    "url": "/Infinity/static/media/software.5b4b5065.jpg"
+  },
+  {
+    "revision": "1d22e223f2ae1a6519b64e7384c3c224",
+    "url": "/Infinity/static/media/web.1d22e223.jpg"
+  }
+]);
